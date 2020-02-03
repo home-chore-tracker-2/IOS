4a1c020ea43dcd37e61c34b35ddc36637dff71a9
Alex Shillingford <alex.shillingford@icloud.com>
2020-02-03 14:32:23 -0700

    Create 10 Unit Test templates TO BE filled out

---
+++ Home Chore Tracker/Home Chore TrackerTests/Home_Chore_TrackerTests.swift
@@ -0,0 +1,52 @@
+//
+//  Home_Chore_TrackerTests.swift
+//  Home Chore TrackerTests
+//
+//  Created by Alex Shillingford on 2/3/20.
+//  Copyright © 2020 HomeChoreTrackeriOSDevs. All rights reserved.
+//
+
+import XCTest
+
+class Home_Chore_TrackerTests: XCTestCase {
+
+    func testCreateUser() {
+        
+    }
+    
+    func testSignIn() {
+        
+    }
+    
+    func testCreateChild() {
+        
+    }
+    
+    func testSignInChild() {
+        
+    }
+    
+    func testCreateChore() {
+        
+    }
+    
+    func testAssignChore() {
+        
+    }
+    
+    func testChildCompleteChore() {
+        
+    }
+    
+    func testParentAssignBonusPointsToChild() {
+        
+    }
+    
+    func testRandomlyAssignChores() {
+        
+    }
+    
+    func testEncodeChoreImage() {
+        
+    }
+}
