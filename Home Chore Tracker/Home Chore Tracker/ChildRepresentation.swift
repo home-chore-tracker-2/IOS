5de2e6c06aeacacaa40e862c584a4ad41ac4d38e
Alex Shillingford <alex.shillingford@icloud.com>
2020-02-03 14:26:04 -0700

    Create Models for Parent, Child, and Chore

---
+++ Home Chore Tracker/Home Chore Tracker/ChildRepresentation.swift
@@ -0,0 +1,15 @@
+//
+//  ChildRepresentation.swift
+//  Home Chore Tracker
+//
+//  Created by Alex Shillingford on 2/3/20.
+//  Copyright © 2020 HomeChoreTrackeriOSDevs. All rights reserved.
+//
+
+import Foundation
+
+struct ChildRepresentation {
+    var id, points, cleanStreak: Int
+    var username, password: String
+    var chores: [ChoreRepresentation]
+}
---
+++ Home Chore Tracker/Home Chore Tracker/ChoreRepresentation.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreRepresentation.swift
+//  Home Chore Tracker
+//
+//  Created by Alex Shillingford on 2/3/20.
+//  Copyright © 2020 HomeChoreTrackeriOSDevs. All rights reserved.
+//
+
+import Foundation
+
+struct ChoreRepresentation {
+    var id, childID, points, bonusPoints: Int
+    var choreName, description: String
+    var dueDate: Date
+    var picture: URL
+    var completed: Bool
+}
